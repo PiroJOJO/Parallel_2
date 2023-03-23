49230f3523f81f43069927b72e609b4cc50805b6
PiroJOJO <sonymitchel5@gmail.com>
2023-03-23 19:16:18 +0000

    Final_version_1

--- sandbox.c
+++ sandbox.c
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <algorithm>
 #include <chrono>
-
 #pragma acc routine seq
-double& get_element(double* vec, size_t n, int i, int i2)
-{
-    return vec[i + n * i2];
-}
-
-void print_matrix(double* vec, size_t n)
-{
-    for (size_t i = 0; i < n; ++i)
-    {
-        for (size_t j = 0; j < n; ++j)
-        {
-            std::cout<<get_element(vec, n, j, i)<<' ';
-        }
-        std::cout<<std::endl;
-    }
-}
-
-#pragma acc routine seq
-inline double steps(size_t n, size_t step, double left, double right)
+double steps(size_t n, size_t step, double left, double right)
 {
     double val = (right - left)/(n - 1);
     return left +  val * step;
 }
 
-int main(int argc, char *argv[]) {
+double** create_matrix(size_t n)
+{
+    double** arr = new double*[n];
+    for (size_t i =0; i < n; ++i)
+    {
+        arr[i] = new double[n];
+    }
+    return arr;
+}
+
+void print_matrix(double** vec, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < n; ++j)
+        {
+            std::cout<<vec[i][j]<<' ';
+        }
+        std::cout<<std::endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
     auto begin = std::chrono::steady_clock::now();
-    if (argc != 7)
+    if(argc != 7)
     {
         std::cout<<"Enter a string like this: Accuracy _ iterations _ size _"<<std::endl;
     }
@@ -37,63 +41,73 @@ int main(int argc, char *argv[]) {
     double error = std::stod(argv[2]);
     size_t iter = std::stoi(argv[4]);
     size_t n = std::stoi(argv[6]);
-    double* vec = new double[n*n];
-    double* new_vec = new double[n*n];
+    double** vec = create_matrix(n);
+    double** new_vec = create_matrix(n);
+    vec[0][0] = 10;
+    vec[0][n - 1] = 20;
+    vec[n - 1][n - 1] = 30;
+    vec[n - 1][0] = 20;
     size_t it = 0;
-    //Заполнение угловых значений
-    get_element(vec, n, 0, 0) = 10;
-    get_element(vec, n, n - 1, 0) = 20;
-    get_element(vec, n, n - 1, n - 1) = 30;
-    get_element(vec, n, 0, n - 1) = 20;
-    //Заполнение рамок матриц
-#pragma acc data copy(vec[:n*n], new_vec[n*n])
+#pragma acc data copy(vec[0:n][0:n], new_vec[0:n][0:n])
     {
-#pragma acc parallel loop
-        for (size_t i = 1; i < n - 1; ++i) {
-            get_element(vec, n, i, 0) = steps(n, i, get_element(vec, n, 0, 0),
-                                              get_element(vec, n, n - 1, 0));
-            get_element(vec, n, i, n - 1) = steps(n, i, get_element(vec, n, 0, n - 1),
-                                                  get_element(vec, n, n - 1, n - 1));
-            get_element(vec, n, 0, i) = steps(n, i, get_element(vec, n, 0, 0),
-                                              get_element(vec, n, 0, n - 1));
-            get_element(vec, n, n - 1, i) = steps(n, i, get_element(vec, n, n - 1, 0),
-                                                  get_element(vec, n, n - 1, n - 1));
+#pragma acc parallel loop independent
+        for (size_t i = 0; i < n; ++i) 
+        {
+            vec[0][i] = steps(n, i, vec[0][0], vec[0][n - 1]);
+            vec[i][n - 1] = steps(n, i, vec[0][n - 1], vec[n - 1][n - 1]);
+            vec[n - 1][i] = steps(n, i, vec[n - 1][0], vec[n - 1][n - 1]);
+            vec[i][0] = steps(n, i, vec[0][0], vec[n - 1][0]);
         }
-        std::copy_n(vec, n * n, new_vec);
-        double max_error = error + 1;             
-        while(error < max_error && it < iter)
-	    { 
-                max_error = 0;
-                it ++;
-#pragma acc data copy(max_error)              
-#pragma acc parallel loop collapse(2) independent reduction(max:max_error)
-                for (int j = 1; j < n - 1; ++j)
-                {
-                    for (int k = 1; k < n - 1; ++k)
-                    {
-                        get_element(new_vec, n, k, j) =
-                                0.25 * (get_element(vec, n, k - 1, j) + get_element(vec, n, k + 1, j)
-                                        + get_element(vec, n, k, j - 1) + get_element(vec, n, k, j + 1));
-                        max_error = std::max(max_error,
-                                             std::abs(get_element(vec, n, k, j) - get_element(new_vec, n, k, j)));
-                    }
-                }
-                #pragma acc parallel loop collapse(2)
-	                for (int j = 1; j < n - 1; ++j) 
-                        for (int k = 1; k < n - 1; ++k) 
-                            get_element(vec, n, k, j) =get_element(new_vec, n, k, j);
+#pragma acc parallel loop independent
+        for (size_t i = 0; i < n; ++i) 
+        {
+            new_vec[0][i] = vec[0][i];
+            new_vec[n - 1][i] = vec[n - 1][i];
+            new_vec[i][0] = vec[i][0];
+            new_vec[i][n - 1] = vec[i][n - 1];
+        }
+	double max_error = error + 1;
+	while(error < max_error && it < iter)
+	{  
+		max_error = 0;
+		it++;
 
-                //vec = new_vec;
+#pragma acc data copy(max_error)
+#pragma acc parallel loop collapse(2) independent reduction(max:max_error)
+            for (int j = 1; j < n - 1; ++j) 
+            {
+                for (int k = 1; k < n - 1; ++k) 
+                {
+                    new_vec[j][k] = 0.25 * (vec[j - 1][k] + vec[j + 1][k] + vec[j][k - 1] + vec[j][k + 1]);
+                    max_error = std::max(max_error,
+                                         std::abs(vec[j][k] - new_vec[j][k]));
+                }
+                //std::copy_n(new_vec[j],n, vec[j]);
             }
-            std::cout<<"Error: "<<max_error<<std::endl;
+ 
+#pragma acc parallel loop independent
+	        for (size_t j = 0; j < n; ++j) 
+            {
+                    double* tmp = vec[j];
+                    vec[j] = new_vec[j];
+                    new_vec[j] = tmp;
+            }
     }
+   
+    std::cout<<"Error: "<<max_error<<std::endl;
+    }
+
     auto end = std::chrono::steady_clock::now();
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end-begin);
     std::cout<<"time: "<<elapsed_ms.count()<<" mcs\n";
     std::cout<<"Iterations: "<<it<<std::endl;
+  //  print_matrix(vec, n);
+    for (size_t i = 0; i < n; i++)
+    {
+        delete [] vec[i];
+        delete [] new_vec[i];
+    }
     delete [] vec;
     delete [] new_vec;
-    //print_matrix(vec, n);
     return 0;
 }
-
